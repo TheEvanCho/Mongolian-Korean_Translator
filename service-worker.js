0165c139642407bd3b57d83861f4157a0956bd2a
Evan Cho <echo0908@Evans-MacBook-Pro.local>
2026-07-03 16:21:07 +0900

    Animatio is weird: revertingRevert "service worker network-first update"
    This reverts commit ba58af774eab2c250dc989822012a2ec807d8d26.

--- service-worker.js
+++ service-worker.js
@@ -1,18 +1,31 @@
-const CACHE = "translator-v2";
+const CACHE = "translator-v1";
 
-const FILES = ["/", "/index.html", "/style.css", "/app.js", "/manifest.json"];
+const FILES = [
+    "/",
+    "/index.html",
+    "/style.css",
+    "/app.js",
+    "/manifest.json"
+];
 
-self.addEventListener("install", (event) => {
-  self.skipWaiting();
-  event.waitUntil(caches.open(CACHE).then((cache) => cache.addAll(FILES)));
+self.addEventListener("install", event => {
+
+    event.waitUntil(
+
+        caches.open(CACHE)
+            .then(cache => cache.addAll(FILES))
+
+    );
+
 });
 
-self.addEventListener("activate", (event) => {
-  event.waitUntil(self.clients.claim());
-});
+self.addEventListener("fetch", event => {
 
-self.addEventListener("fetch", (event) => {
-  event.respondWith(
-    fetch(event.request).catch(() => caches.match(event.request)),
-  );
+    event.respondWith(
+
+        caches.match(event.request)
+            .then(response => response || fetch(event.request))
+
+    );
+
 });
