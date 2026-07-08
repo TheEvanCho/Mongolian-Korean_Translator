2e9ff085d2a6d273204727db22df218806a4563b
Evan Cho <echo0908@Evans-MacBook-Pro.local>
2026-07-08 14:32:20 +0900

    fixed errors, resized SVG, fixed MODEL sstopping js

--- app.js
+++ app.js
@@ -15,7 +15,6 @@ const DEVICE = isIOS ? "ios" : "desktop";
 console.log("Device:", DEVICE);
 
 console.log("iOS:", isIOS);
-console.log("Model:", MODEL);
 
 let translatorManager = {
   engine: null,
@@ -180,11 +179,10 @@ c-5.857-5.857-15.355-5.857-21.213,0l-33.098,33.098c-10.447,0.662-18.723,9.32-18.
 c-0.002,11.045,8.951,20,19.998,20.002l0,0C164.998,216.89,164.998,216.89,165,216.89z"/> 
 </svg>`;
 
-const BOOK_ICON = `<svg 
-width="800px" 
-height="800px" 
-viewBox="0 0 24 24" 
-fill="none" 
+const BOOK_ICON = `<svg
+class="deepIcon"
+viewBox="0 0 24 24"
+fill="none"
 xmlns="http://www.w3.org/2000/svg">
 <path d="M4 19V6.2C4 5.0799 4 4.51984 4.21799 4.09202C4.40973 3.71569 4.71569 3.40973 5.09202 3.21799C5.51984 3 6.0799 3 7.2 3H16.8C17.9201 3 18.4802 3 18.908 3.21799C19.2843 3.40973 19.5903 3.71569 19.782 4.09202C20 4.51984 20 5.0799 20 6.2V17H6C4.89543 17 4 17.8954 4 19ZM4 19C4 20.1046 4.89543 21 6 21H20M9 7H15M9 11H15M19 17V21" stroke="#000000" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
 </svg>`;
